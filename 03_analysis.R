f2210fe22b44442c8e0497360fa52ea40c75ffff
Kaija Gahm <kgahm@ucla.edu>
2023-03-15 13:16:09 -0700

    new script just for relating movement to social

---
+++ 03_analysis.R
@@ -0,0 +1,10 @@
+# Analysis script for movement/social position
+
+# Setup -------------------------------------------------------------------
+## Load packages -----------------------------------------------------------
+library(vultureUtils)
+library(sf)
+library(tidyverse)
+library(igraph)
+library(mapview) # for quick maps
+library(factoextra)
