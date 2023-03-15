2a83c06770d13178eb00fe1da09af8b5d88c0703
Kaija Gahm <kgahm@ucla.edu>
2023-03-15 15:27:00 -0700

    incorporated elevations and re-saved

--- 00_dataPrep.R
+++ 00_dataPrep.R
@@ -29,10 +29,10 @@ datAnnotCleaned <- datAnnotCleaned %>%
                                   TRUE ~ paste(season, year, sep = "_")))
 
 # Separate the seasons -----------------------------
-seasonNames <- map_chr(seasons, ~.x$seasonUnique[1])
 seasons <- datAnnotCleaned %>%
   group_by(seasonUnique) %>%
   group_split(.keep = T)
+seasonNames <- map_chr(seasons, ~.x$seasonUnique[1])
 
 # Restrict to southern individuals ----------------------------------------
 # Based on previous investigations for the 2022 breeding and non-breeding seasons, have found that a good cutoff for southern vs. non-southern is 3550000 (in ITM)
@@ -68,7 +68,7 @@ seasons <- map(seasons, ~.x %>%
 
 # Include only individuals with enough days tracked -----------------------
 ## Must be tracked for at least 1/3 of the number of days in the season.
-durs <- map_dbl(seasons, ~as.numeric(difftime(max(.x$dateOnly), min(.x$dateOnly), units = "days")))
+durs <- map_dbl(seasons, ~length(unique(.x$dateOnly)))
 
 indivsToKeep <- map2(.x = seasons, .y = durs, ~.x %>%
                        st_drop_geometry() %>%
@@ -81,9 +81,31 @@ indivsToKeep <- map2(.x = seasons, .y = durs, ~.x %>%
 ## remove the individuals not tracked for long enough
 seasons <- map2(.x = seasons, .y = indivsToKeep, ~.x %>% filter(trackId %in% .y))
 
+
+# Get number of days tracked for each individual --------------------------
+daysTracked_seasons <- map2(seasons, durs, ~.x %>%
+                             st_drop_geometry() %>%
+                             group_by(trackId) %>%
+                             summarize(daysTracked = length(unique(dateOnly)),
+                                       propDaysTracked = daysTracked/.y))
+
 # Get roosts for each season ----------------------------------------------
-roosts_seasons <- purrr::map(seasons, ~vultureUtils::get_roosts_df(df = .x, id = "trackId"))
+roosts_seasons <- purrr::map(seasons, ~vultureUtils::get_roosts_df(df = .x, id = "trackId")) 
+
+roosts_seasons <- roosts_seasons %>%
+  map(., ~st_as_sf(.x, crs = "WGS84", coords = c("location_long", "location_lat")))
+
+# Get elevation rasters ---------------------------------------------------
+elevs_z09_seasons <- map(seasons, ~elevatr::get_elev_raster(.x %>% st_transform(crs = "WGS84"), z = 9))
+
+groundElev_z09 <- map2(elevs_z09_seasons, seasons, ~raster::extract(x = .x, y = .y %>% st_transform(crs = "WGS84")))
+
+seasons <- map2(.x = seasons, .y = groundElev_z09, 
+                ~.x %>% mutate(height_above_ground = .y,
+                               height_above_ground = case_when(height_above_ground <0 ~ 0,
+                                                               TRUE ~ height_above_ground)))
 
 # Export the data
 save(seasons, file = "data/seasons.Rda")
+save(daysTracked_seasons, file = "data/daysTracked_seasons.Rda")
 save(roosts_seasons, file = "data/roosts_seasons.Rda")
