ca9efe510590c1d0fa5cd1677b9a3a65cc831947
Kaija Gahm <kgahm@ucla.edu>
2023-04-21 13:32:19 -0700

    Some fixes for age and sex

--- 00_dataPrep.R
+++ 00_dataPrep.R
@@ -16,58 +16,79 @@ library(readxl)
 
 # dat <- vultureUtils::downloadVultures(loginObject = MB.LoginObject, removeDup = T, dfConvert = T, quiet = T, dateTimeStartUTC = "2021-12-01 00:00", dateTimeEndUTC = "2023-03-31 11:59")
 # write_feather(dat, "data/dat.feather")
-# dat <- read_feather("data/dat.feather")
-# ## fix trackId
-# dat <- dat %>%
-#   mutate(trackId = as.character(trackId),
-#          trackId = case_when(trackId == "E03" ~ "E03w",
-#                              TRUE ~ trackId))
-# 
-# # Add the Nili_id
-# ww <- read_excel("data/whoswho_vultures_20230315_new.xlsx", sheet = "all gps tags")[,1:35] %>%
-#   dplyr::select(Nili_id, Movebank_id) %>%
-#   distinct()
-# 
-# all(dat$trackId %in% ww$Movebank_id) # true
-# 
-# dat2 <- left_join(dat, ww, by = c("trackId" = "Movebank_id"))
-# 
-# ## annotate the data with periods to remove
-# toRemove <- read_excel("data/whoswho_vultures_20230315_new.xlsx", sheet = "periods_to_remove")
-# 
-# toRemove <- toRemove %>%
-#   dplyr::select(Nili_id,
-#                 "trackId" = Movebank_id,
-#                 remove_start,
-#                 remove_end,
-#                 reason) %>%
-#   mutate(across(c(remove_start, remove_end), .fns = function(x){
-#     lubridate::ymd(x)
-#   })) %>%
-#   dplyr::filter(!is.na(remove_end))
-# 
-# toRemove_long <- toRemove %>%
-#   group_by(Nili_id, reason) %>%
-#   # sequence of daily dates for each corresponding start, end elements
-#   dplyr::mutate(dateOnly = map2(remove_start, remove_end, seq, by = "1 day")) %>%
-#   # unnest the list column
-#   unnest(cols = c(dateOnly)) %>% 
-#   # remove any duplicate rows
-#   distinct() %>%
-#   dplyr::select(-c(remove_start, remove_end)) %>%
-#   rename("status" = reason)
-# 
-# # Join to the original data
-# datAnnot <- dat2 %>%
-#   left_join(toRemove_long, by = c("Nili_id", "dateOnly")) %>%
-#   mutate(status = replace_na(status, "valid"))
-# nrow(datAnnot) == nrow(dat2) #T
-# 
-# # Clean the data
-# ## Region masking, downsampling, removal of speed outliers, setting altitude outliers to NA, etc.
-# mask <- sf::st_read("data/CutOffRegion.kml")
-# datAnnotCleaned <- vultureUtils::cleanData(dataset = datAnnot, mask = mask, inMaskThreshold = 0.33, removeVars = F, idCol = "Nili_id", downsample = T)
-# save(datAnnotCleaned, file = "data/datAnnotCleaned.Rda")
+dat <- read_feather("data/dat.feather")
+## fix trackId
+dat <- dat %>%
+  mutate(trackId = as.character(trackId),
+         trackId = case_when(trackId == "E03" ~ "E03w",
+                             TRUE ~ trackId))
+
+# Add the Nili_id
+ww <- read_excel("data/whoswho_vultures_20230315_new.xlsx", sheet = "all gps tags")[,1:35] %>%
+  dplyr::select(Nili_id, Movebank_id) %>%
+  distinct()
+
+all(dat$trackId %in% ww$Movebank_id) # true
+
+dat2 <- left_join(dat, ww, by = c("trackId" = "Movebank_id"))
+
+## annotate the data with periods to remove
+toRemove <- read_excel("data/whoswho_vultures_20230315_new.xlsx", sheet = "periods_to_remove")
+
+toRemove <- toRemove %>%
+  dplyr::select(Nili_id,
+                remove_start,
+                remove_end,
+                reason) %>%
+  mutate(across(c(remove_start, remove_end), .fns = function(x){
+    lubridate::ymd(x)
+  })) %>%
+  dplyr::filter(!is.na(remove_end))
+
+toRemove_long <- toRemove %>%
+  group_by(Nili_id, reason) %>%
+  # sequence of daily dates for each corresponding start, end elements
+  dplyr::mutate(dateOnly = map2(remove_start, remove_end, seq, by = "1 day")) %>%
+  # unnest the list column
+  unnest(cols = c(dateOnly)) %>%
+  # remove any duplicate rows
+  distinct() %>%
+  dplyr::select(-c(remove_start, remove_end)) %>%
+  rename("status" = reason)
+
+# Join to the original data
+datAnnot <- dat2 %>%
+  left_join(toRemove_long, by = c("Nili_id", "dateOnly")) %>%
+  mutate(status = replace_na(status, "valid"))
+nrow(datAnnot) == nrow(dat2) #T
+
+# Attach age and sex information
+as <- read_excel("data/whoswho_vultures_20230315_new.xlsx", sheet = "all gps tags")[,1:35] %>%
+  dplyr::select(Nili_id, birth_year, sex) %>%
+  distinct()
+
+datAnnot2 <- datAnnot %>%
+  dplyr::select(-c("sex")) %>%
+  left_join(as, by = "Nili_id")
+nrow(datAnnot2) == nrow(datAnnot)
+
+datAnnot3 <- datAnnot2 %>%
+  mutate(age = lubridate::year(timestamp) - birth_year,
+         age_group = case_when(age == 0 ~ "juv",
+                               age >= 1 & age <= 4 ~ "sub",
+                               age >= 5 ~ "adult",
+                               TRUE ~ NA),
+         sex = case_when(sex == "f/m" ~ "u",
+                         TRUE ~ sex))
+
+datAnnot <- datAnnot3 # a hack so the rest of the code will work
+
+# Clean the data
+## Region masking, downsampling, removal of speed outliers, setting altitude outliers to NA, etc.
+mask <- sf::st_read("data/CutOffRegion.kml")
+# XXX START HERE--will need to re-download vultureUtils, I think.
+datAnnotCleaned <- vultureUtils::cleanData(dataset = datAnnot, mask = mask, inMaskThreshold = 0.33, removeVars = F, idCol = "Nili_id", downsample = T)
+save(datAnnotCleaned, file = "data/datAnnotCleaned.Rda")
 
 ## Load data ---------------------------------------------------------------
 load("data/datAnnotCleaned.Rda")
