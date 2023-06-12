a101f077ef75627444cecd425eed8200f8ca2ec1
Kaija Gahm <kgahm@ucla.edu>
2023-06-12 11:00:49 -0700

    added evenness calculations

--- 02_calcSocial.R
+++ 02_calcSocial.R
@@ -4,17 +4,19 @@ library(vultureUtils)
 library(igraph)
 library(tidyverse)
 library(sf)
+source("evenness.R")
 
 ## Data ---------------------------------------------------------------
 load("data/seasons_forSoc.Rda")
+seasons_forSoc <- map(seasons_forSoc, ~st_as_sf(.x, coords = c("location_long", "location_lat"), crs = "WGS84", remove = F))
 roostPolygons <- sf::st_read("data/roosts50_kde95_cutOffRegion.kml")
-load("data/roosts_seasons_forSoc.Rda")
+load("data/roosts_seasons.Rda")
 seasonNames <- map_chr(seasons_forSoc, ~as.character(.x$seasonUnique[1]))
 
 # Social Networks ---------------------------------------------------------
-flightSeasons <- map(seasons_forSoc, ~vultureUtils::getFlightEdges(.x, roostPolygons = roostPolygons, distThreshold = 1000, idCol = "Nili_id", return = "sri")) # XXX start here
-feedingSeasons <- map(seasons_forSoc, ~vultureUtils::getFeedingEdges(.x, roostPolygons = roostPolygons, distThreshold = 50, return = "sri"))
-roostSeasons <- map(roosts_seasons_forSoc, ~vultureUtils::getRoostEdges(.x, mode = "polygon", roostPolygons = roostPolygons, return = "sri", latCol = "location_lat", longCol = "location_long", idCol = "Nili_id", dateCol = "roost_date"))
+flightSeasons <- map(seasons_forSoc, ~vultureUtils::getFlightEdges(.x, roostPolygons = roostPolygons, distThreshold = 1000, idCol = "Nili_id", return = "sri"))
+feedingSeasons <- map(seasons_forSoc, ~vultureUtils::getFeedingEdges(.x, roostPolygons = roostPolygons, distThreshold = 50, return = "sri")) # XXX start here
+roostSeasons <- map(roosts_seasons, ~vultureUtils::getRoostEdges(.x, mode = "polygon", roostPolygons = roostPolygons, return = "sri", latCol = "location_lat", longCol = "location_long", idCol = "Nili_id", dateCol = "roost_date"))
 
 flightSeasons_g <- map(flightSeasons, ~vultureUtils::makeGraph(mode = "sri", data = .x, weighted = T))
 feedingSeasons_g <- map(feedingSeasons,  ~vultureUtils::makeGraph(mode = "sri", data = .x, weighted = T))
@@ -24,6 +26,7 @@ networkMetrics <- map2_dfr(flightSeasons_g, seasonNames, ~{
   df <- data.frame(degree = igraph::degree(.x),
                    strength = igraph::strength(.x),
                    pageRank = igraph::page_rank(.x)$vector,
+                   evenness = evenness(.x),
                    Nili_id = names(degree(.x))) %>%
     bind_cols(season = .y,
               type = "flight")
@@ -34,6 +37,7 @@ networkMetrics <- map2_dfr(flightSeasons_g, seasonNames, ~{
     df <- data.frame(degree = igraph::degree(.x),
                      strength = igraph::strength(.x),
                      pageRank = igraph::page_rank(.x)$vector,
+                     evenness = evenness(.x),
                      Nili_id = names(degree(.x))) %>%
       bind_cols(season = .y,
                 type = "feeding")
@@ -44,10 +48,11 @@ networkMetrics <- map2_dfr(flightSeasons_g, seasonNames, ~{
     df <- data.frame(degree = igraph::degree(.x),
                      strength = igraph::strength(.x),
                      pageRank = igraph::page_rank(.x)$vector,
+                     evenness = evenness(.x),
                      Nili_id = names(degree(.x))) %>%
       bind_cols(season = .y,
                 type = "roosting")
-    
+
     return(df)
   }))
 
@@ -59,8 +64,9 @@ networkMetrics <- networkMetrics %>%
   mutate(degreeRelative = degree/n,
          strengthRelative = strength/n,
          pageRankRelative = pageRank/n,
-         sbd = strength/degree) %>%
-  dplyr::select(season, type, n, Nili_id, degree, degreeRelative, strength, strengthRelative, sbd, pageRank, pageRankRelative)
+         sbd = strength/degree,
+         evenness = evenness) %>%
+  dplyr::select(season, type, n, Nili_id, degree, degreeRelative, strength, strengthRelative, sbd, pageRank, pageRankRelative, evenness)
 
 # Save network metrics ----------------------------------------------------
 save(networkMetrics, file = "data/networkMetrics.Rda")
