3f55325b3b4f709d7b4280701189e0d7b4668fb3
kaija <kgahm@g.ucla.edu>
2023-07-31 19:32:16 +0000

    back to loading tidyverse. Removed covariate plots.

--- 05_visualize.R
+++ 05_visualize.R
@@ -1,11 +1,4 @@
-# In lieu of tidyverse
-library(ggplot2)
-library(tidyr)
-library(stringr)
-library(readr)
-library(dplyr)
-library(tibble)
-library(purrr)
+library(tidyverse)
 #
 library(jtools)
 library(sjPlot)
@@ -26,124 +19,6 @@ situationColors <- c("dodgerblue2", "olivedrab3", "gold") # flight, roosting, fe
 flightColor <- "dodgerblue2"
 roostingColor <- "olivedrab3"
 feedingColor <- "gold"
-
-# Examine covariates ------------------------------------------------------
-# SEX
-# ## differences in PC values by sex
-# long <- linked %>%
-#   filter(sex %in% c("f", "m")) %>%
-#   dplyr::select(Nili_id, season, age, age_group, sex, PC1, PC2, PC3) %>%
-#   pivot_longer(cols = c("PC1", "PC2", "PC3"), values_to = "value", 
-#                names_to = "PC")
-# stat.test <- long %>%
-#   group_by(PC) %>%
-#   t_test(value ~ sex) %>%
-#   adjust_pvalue(method = "bonferroni") %>%
-#   add_significance() %>% 
-#   add_xy_position(x = "sex")
-# 
-# bxp <- ggboxplot(
-#   long, x = "sex", y = "value", 
-#   fill = "sex", 
-#   palette = c("orange", "skyblue3"),
-#   facet.by = "PC"
-# ) + stat_pvalue_manual(stat.test)
-# bxp
-# 
-# linked %>%
-#   dplyr::select(sex, type, Nili_id, degreeRelative, strengthRelative, evenness) %>%
-#   pivot_longer(cols = c("degreeRelative", "strengthRelative", "evenness"), 
-#                names_to = "socialPositionMeasure", values_to = "value") %>%
-#   filter(!is.na(sex)) %>%
-#   ggplot(aes(x = sex, y = value, fill = sex))+
-#   geom_boxplot()+
-#   theme_minimal()+
-#   scale_fill_manual(values = c("orange", "skyblue3"))+
-#   theme(legend.position = "none")+
-#   facet_wrap(facets = vars(type, socialPositionMeasure), 
-#              nrow = 3, ncol = 3, scales= "free")+
-#   ylab("Value")+
-#   xlab("Sex")
-# 
-# # AGE
-# ## differences in PC values by age
-# long %>%
-#   ggplot(aes(x = age, y = value, col = PC))+
-#   geom_point(size = 1.5, alpha = 0.7)+
-#   geom_smooth(method = "lm")+
-#   scale_color_viridis_d(option = "D")+
-#   theme(text = element_text(size = 18))+
-#   ylab("Value")+
-#   xlab("Age")
-# 
-# stat.test_age <- long %>%
-#   group_by(PC) %>%
-#   t_test(value ~ age_group) %>%
-#   adjust_pvalue(method = "bonferroni") %>%
-#   add_significance() %>% 
-#   add_xy_position(x = "age_group")
-# 
-# bxp_age <- ggboxplot(
-#   long, x = "age_group", y = "value", 
-#   fill = "age_group", 
-#   palette = c("red", "red4"),
-#   facet.by = "PC"
-# ) + stat_pvalue_manual(stat.test_age)
-# bxp_age
-# 
-# ## differences in degree/strength by age 
-# head(linked)
-# linked %>%
-#   dplyr::select(age_group, type, Nili_id, degreeRelative, strengthRelative, evenness) %>%
-#   pivot_longer(cols = c("degreeRelative", "strengthRelative", "evenness"), names_to = "socialPositionMeasure", values_to = "value") %>%
-#   filter(!is.na(age_group)) %>%
-#   ggplot(aes(x = age_group, y = value, fill = age_group))+
-#   geom_boxplot()+
-#   theme_minimal()+
-#   scale_fill_manual(values = c("red", "red4"))+
-#   theme(legend.position = "none")+
-#   facet_wrap(facets = vars(type, socialPositionMeasure), 
-#              nrow = 3, ncol = 3, scales= "free")+
-#   ylab("Value")+
-#   xlab("Age group")
-# 
-# # SEASON
-# linked %>%
-#   dplyr::select(type, Nili_id, degreeRelative, strengthRelative, evenness, season) %>%
-#   pivot_longer(cols = c("degreeRelative", "strengthRelative", "evenness"), names_to = "socialPositionMeasure", values_to = "value") %>%
-#   filter(!is.na(season)) %>%
-#   ggplot(aes(x = season, y = value, fill = season))+
-#   geom_boxplot()+
-#   theme_minimal()+
-#   scale_fill_manual(values = seasonColors)+
-#   theme(legend.position = "none")+
-#   facet_wrap(facets = vars(type, socialPositionMeasure), 
-#              nrow = 3, ncol = 3, scales= "free")+
-#   ylab("Value")+
-#   xlab("Season") 
-# 
-# ## differences in PC values by season
-# long <- linked %>%
-#   dplyr::select(Nili_id, season, age, age_group, sex, PC1, PC2, PC3) %>%
-#   pivot_longer(cols = c("PC1", "PC2", "PC3"), values_to = "value", 
-#                names_to = "PC")
-# stat.test <- long %>%
-#   group_by(PC) %>%
-#   t_test(value ~ season) %>%
-#   adjust_pvalue(method = "bonferroni") %>%
-#   add_significance() %>% 
-#   add_xy_position(x = "season")
-# 
-# bxp <- ggboxplot(
-#   long, x = "season", y = "value", 
-#   fill = "season", 
-#   palette = seasonColors,
-#   facet.by = "PC"
-# ) + stat_pvalue_manual(stat.test)
-# bxp # we definitely have seasonal differences in movement! As expected.
-
-# Effect size plots
-# Here are some plots made with jtools, following the vignette here: https://cran.r-project.org/web/packages/jtools/vignettes/summ.html
 
 # Degree
 plot_model(mods[["d"]])
@@ -170,6 +45,12 @@ ggplot(deg_pc2, aes(x, predicted))+
   theme(text = element_text(size = 16))
 
 # Strength
+plot_model(mods[["s"]], type = "pred", terms = c("PC1", "age_group", "situ"), 
+           show.data = T)+
+  geom_point(size = 0.5)
+plot_model(mods[["s"]], type = "pred", terms = c("age_group", "situ"))
+
+
 str_pc1 <- ggpredict(mods[["s"]], terms = c("PC1", "season", "situ"))
 ggplot(str_pc1, aes(x, predicted))+
   geom_line(linewidth = 1.5, aes(col = group))+
