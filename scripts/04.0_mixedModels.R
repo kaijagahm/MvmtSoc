4fd380d33c6d233aa91e3de87f2229ca47749937
Kaija Gahm <kaija.gahm@gmail.com>
2024-04-15 12:42:06 -0700

    removed centrality portions

--- scripts/04.0_mixedModels.R
+++ scripts/04.0_mixedModels.R
@@ -58,7 +58,7 @@ linked %>%
 # Degree ------------------------------------------------------------------
 tar_load(allMetrics)
 vultures <- sample(unique(allMetrics$Nili_id), 10)
-allMetrics %>% filter(Nili_id %in% vultures) %>% ggplot(aes(x = Nili_id, y = wrapped_normDegree, group = interaction(situ, Nili_id), col = situ))+geom_violin()+facet_wrap(~season, scales = "free")+geom_point(aes(x = Nili_id, y = normDegree)) # individuals generally *do* have degrees higher than expected by chance.
+allMetrics %>% filter(Nili_id %in% vultures) %>% ggplot(aes(x = Nili_id, y = wrapped_degree, group = interaction(situ, Nili_id), col = situ))+geom_violin()+facet_wrap(~season, scales = "free")+geom_point(aes(x = Nili_id, y = degree)) # individuals generally *do* have degrees higher than expected by chance.
 
 degree_base <- glmmTMB(z_deg ~ situ + movement + roost_div + space_use + age_group + season + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian()) 
 check_predictions(degree_base)
@@ -86,16 +86,16 @@ summary(degree_3) # season*space, movement*season, and situ*movement are all non
 check_collinearity(degree_3) # the ones with the highest collinearity have some significant effects. Maybe we can reduce the collinearity by removing non-significant ones instead? Let's start by removing season*space
 
 degree_4 <- glmmTMB(z_deg ~ situ*movement + movement*season + situ*roost_div + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian())
-summary(degree_4) # 
+summary(degree_4) #
 check_collinearity(degree_4) # situ*space has high collinearity but also a significant effect. Maybe we could remove situ*movement instead...
 
 degree_5 <- glmmTMB(z_deg ~ movement*season + situ*roost_div + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian())
-summary(degree_5) # 
+summary(degree_5) #
 check_collinearity(degree_5) # it's not going to help, but we can remove movement*season last...
 
 degree_6 <- glmmTMB(z_deg ~ movement + season + situ*roost_div + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian())
-summary(degree_6) # 
-check_collinearity(degree_6) # these VIFs of 6.55 and 6.65 for situ*roost and situ*space are kind of borderline. One thing we could do is make two models and see if they agree, each removing one of the terms?
+summary(degree_6) #
+check_collinearity(degree_6) # these VIFs of 6.52 and 6.65 for situ*roost and situ*space are kind of borderline. One thing we could do is make two models and see if they agree, each removing one of the terms?
 
 degree_7a <- glmmTMB(z_deg ~ movement + season + situ*roost_div + space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian())
 summary(degree_7a) # lo and behold, the other is now not significant at all...
@@ -119,12 +119,12 @@ plot_d_eff_movement <- ggplot(d_eff_movement, aes(x, predicted))+
   geom_point(data = linked, aes(x = movement, y = z_deg, col = situ), alpha = 0.5)+
   geom_line(linewidth = 1, col = "black", linetype = 2)+
   scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
-  ylab("Normalized degree (deviation from random)")+
+  ylab("Degree deviation")+
   xlab("Movement")+
   ggtitle("")+theme_classic()+
   theme(text = element_text(size = 16))
 plot_d_eff_movement
-ggsave(plot_d_eff_movement, file = here("fig/mmPlots/plot_d_eff_movement.png"), width = 7, height = 6)
+#ggsave(plot_d_eff_movement, file = here("fig/mmPlots/plot_d_eff_movement.png"), width = 7, height = 6)
 
 ## space_use (main) --------------------------------------------------------
 d_eff_space <- as.data.frame(ggeffect(degree_mod, terms = c("space_use")))
@@ -134,12 +134,12 @@ plot_d_eff_space <- ggplot(d_eff_space, aes(x, predicted))+
   geom_point(data = linked, aes(x = space_use, y = z_deg, col = situ), alpha = 0.5)+
   geom_line(linewidth = 1, col = "black", linetype = 2)+
   scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
-  ylab("Normalized degree (deviation from random)")+
+  ylab("Degree deviation")+
   xlab("Space use (log-transformed)")+
   ggtitle("")+theme_classic()+
   theme(text = element_text(size = 16))
 plot_d_eff_space
-ggsave(plot_d_eff_space, file = here("fig/mmPlots/plot_d_eff_space.png"), width = 7, height = 6)
+#ggsave(plot_d_eff_space, file = here("fig/mmPlots/plot_d_eff_space.png"), width = 7, height = 6)
 
 ## space_use:situ ----------------------------------------------------------
 d_eff_situ_space <- as.data.frame(ggeffect(degree_mod, terms = c("space_use", "situ")))
@@ -150,12 +150,12 @@ plot_d_eff_situ_space <- ggplot(d_eff_situ_space, aes(x, predicted))+
   geom_line(aes(col = group), linewidth = 1)+
   scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
   scale_fill_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
-  ylab("Normalized degree (deviation from random)")+
+  ylab("Degree deviation")+
   xlab("Space use (log-transformed)")+
   ggtitle("")+theme_classic()+
   theme(text = element_text(size = 16))
 plot_d_eff_situ_space
-ggsave(plot_d_eff_situ_space, file = here("fig/mmPlots/plot_d_eff_situ_space.png"), width = 7, height = 6)
+#ggsave(plot_d_eff_situ_space, file = here("fig/mmPlots/plot_d_eff_situ_space.png"), width = 7, height = 6)
 
 d_emt_situ_space <- emmeans::emtrends(degree_mod, "situ", var = "space_use") %>%
   as.data.frame() %>%
@@ -172,11 +172,11 @@ plot_d_emt_situ_space <- d_emt_situ_space %>%
   geom_hline(aes(yintercept = 0), linetype = 2)+
   scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
   theme(text = element_text(size = 20), legend.position = "none")+
-  ylab("Space use effect (normalized degree deviation from random)")+
+  ylab("Space use effect (degree deviation)")+
   xlab("Situation")+
   coord_flip()
 plot_d_emt_situ_space
-ggsave(plot_d_emt_situ_space, file = here("fig/mmPlots/plot_d_emt_situ_space.png"), width = 5, height = 6)
+#ggsave(plot_d_emt_situ_space, file = here("fig/mmPlots/plot_d_emt_situ_space.png"), width = 5, height = 6)
 
 ## roost_div (main) --------------------------------------------------------
 d_eff_roost <- as.data.frame(ggeffect(degree_mod, terms = "roost_div"))
@@ -186,21 +186,21 @@ plot_d_eff_roost <- ggplot(d_eff_roost, aes(x, predicted))+
   geom_point(data = linked, aes(x = roost_div, y = z_deg, col = situ), alpha = 0.5)+
   geom_line(linewidth = 1)+
   scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
-  ylab("Normalized degree (deviation from random)")+
+  ylab("Degree deviation")+
   xlab("Roost diversification")+
   ggtitle("")+theme_classic()+
   theme(text = element_text(size = 16))
 plot_d_eff_roost
-ggsave(plot_d_eff_roost, file = here("fig/mmPlots/plot_d_eff_roost.png"), width = 7, height = 6)
+#ggsave(plot_d_eff_roost, file = here("fig/mmPlots/plot_d_eff_roost.png"), width = 7, height = 6)
 
 #@@@@@@@@@@@@@@@@@@@@@
 
 # Strength ----------------------------------------------------------------
 tar_load(allMetrics)
 vultures <- sample(unique(allMetrics$Nili_id), 10)
-allMetrics %>% filter(Nili_id %in% vultures) %>% ggplot(aes(x = Nili_id, y = wrapped_normStrength, group = interaction(situ, Nili_id), col = situ))+geom_violin()+facet_wrap(~season, scales = "free")+geom_point(aes(x = Nili_id, y = normStrength)) # individuals don't really seem to have strengths higher than expected by chance!
+allMetrics %>% filter(Nili_id %in% vultures) %>% ggplot(aes(x = Nili_id, y = wrapped_strength, group = interaction(situ, Nili_id), col = situ))+geom_violin()+facet_wrap(~season, scales = "free")+geom_point(aes(x = Nili_id, y = strength)) # individuals don't really seem to have strengths higher than expected by chance!
 
-strength_base <- glmmTMB(z_str ~ situ + movement + roost_div + space_use + age_group + season + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian()) 
+strength_base <- glmmTMB(z_str ~ situ + movement + roost_div + space_use + age_group + season + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian())
 check_predictions(strength_base) # oof, that looks bad...
 simulationOutput <- DHARMa::simulateResiduals(strength_base)
 plot(simulationOutput, pch=".")
@@ -211,22 +211,22 @@ plotResiduals(simulationOutput) # these look very bad, honestly...
 strength_full <- glmmTMB(z_str ~ situ*movement*season + situ*roost_div*season + situ*space_use*season + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian()) # this will almost certainly have very high VIFs
 check_collinearity(strength_full) # YIKES. Let's get rid of situ*movement*season and situ*season*roost_div for starters.
 
-strength_1 <- glmmTMB(z_str ~ situ*movement + movement*season + situ*roost_div + situ*season + roost_div*season + situ*space_use*season + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian()) 
-check_collinearity(strength_1) # remove situ*season now
+strength_1 <- glmmTMB(z_str ~ situ*movement + movement*season + situ*roost_div + situ*season + roost_div*season + situ*space_use*season + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian())
+check_collinearity(strength_1) # remove situ*season*space now
 
-strength_2 <- glmmTMB(z_str ~ situ*movement + movement*season + situ*roost_div + roost_div*season + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian()) 
+strength_2 <- glmmTMB(z_str ~ situ*movement + movement*season + situ*roost_div + roost_div*season + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian())
 check_collinearity(strength_2) # now we can remove movement*season, assuming it's not highly significant
 summary(strength_2) # lol nope def n.s.
 
-strength_3 <- glmmTMB(z_str ~ situ*movement + situ*roost_div + roost_div*season + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian()) 
+strength_3 <- glmmTMB(z_str ~ situ*movement + situ*roost_div + roost_div*season + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian())
 check_collinearity(strength_3) # highest collinearity with situ*roost
 summary(strength_3) # but it's significant. Let's remove situ*movement instead, and/or roost*season
 
-strength_4 <- glmmTMB(z_str ~ movement + situ*roost_div + roost_div*season + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian()) 
+strength_4 <- glmmTMB(z_str ~ movement + situ*roost_div + roost_div*season + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian())
 check_collinearity(strength_4)
 summary(strength_4) # can remove roost*season
 
-strength_5 <- glmmTMB(z_str ~ movement + situ*roost_div + season + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian()) 
+strength_5 <- glmmTMB(z_str ~ movement + situ*roost_div + season + situ*space_use + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian())
 check_collinearity(strength_5) # once again, we're down to situ*roost and situ*space_use. Let's do the split thing again...
 summary(strength_5)
 
@@ -234,11 +234,11 @@ strength_6a <- glmmTMB(z_str ~ movement + situ*roost_div + season + space_use + 
 check_collinearity(strength_6a) # this is all better
 summary(strength_6a) # highly significant interaction between situ and roost_div
 
-strength_6b <- glmmTMB(z_str ~ movement + situ*space_use + season + roost_div + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian()) 
-check_collinearity(strength_6b) # likewise, much better
-summary(strength_6b) # highly significant interaction between situ and roost_div
-
-compare_performance(strength_6a, strength_6b) # these are basically the same in terms of fit, too.
+# strength_6b <- glmmTMB(z_str ~ movement + situ*space_use + season + roost_div + age_group + (1|seasonUnique)+(1|Nili_id), data = linked, family = gaussian()) 
+# check_collinearity(strength_6b) # likewise, much better
+# summary(strength_6b) # highly significant interaction between situ and roost_div
+# 
+# compare_performance(strength_6a, strength_6b) # these are basically the same in terms of fit, too.
 
 check_predictions(strength_6a) # this still looks really bad
 simulationOutput <- DHARMa::simulateResiduals(strength_6a)
@@ -255,7 +255,7 @@ s_eff_space <- as.data.frame(ggeffect(strength_mod, terms = c("space_use")))
 plot_s_eff_space <- ggplot(s_eff_space, aes(x, predicted))+
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high),
               alpha = 0.2, linewidth = 0.6, show.legend = F)+
-  geom_point(data = linked, aes(x = space_use, y = normStrength, col = situ), alpha = 0.5)+
+  geom_point(data = linked, aes(x = space_use, y = z_str, col = situ), alpha = 0.5)+
   geom_line(linewidth = 1, col = "black", linetype = 2)+
   scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
   ylab("Strength (normalized)")+
@@ -263,13 +263,13 @@ plot_s_eff_space <- ggplot(s_eff_space, aes(x, predicted))+
   ggtitle("")+theme_classic()+
   theme(text = element_text(size = 16))
 plot_s_eff_space
-ggsave(plot_s_eff_space, file = here("fig/mmPlots/plot_s_eff_space.png"), width = 7, height = 6)
+#ggsave(plot_s_eff_space, file = here("fig/mmPlots/plot_s_eff_space.png"), width = 7, height = 6)
 ## space_use:situ ----------------------------------------------------------
 s_eff_situ_space <- as.data.frame(ggeffect(strength_mod, terms = c("space_use", "situ")))
 plot_s_eff_situ_space <- ggplot(s_eff_situ_space, aes(x, predicted))+
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = group),
               alpha = 0.2, linewidth = 0.6, show.legend = F)+
-  geom_point(data = linked, aes(x = space_use, y = normStrength, col = situ), alpha = 0.5)+
+  geom_point(data = linked, aes(x = space_use, y = z_str, col = situ), alpha = 0.5)+
   geom_line(aes(col = group), linewidth = 1)+
   scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
   scale_fill_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
@@ -278,7 +278,7 @@ plot_s_eff_situ_space <- ggplot(s_eff_situ_space, aes(x, predicted))+
   ggtitle("")+theme_classic()+
   theme(text = element_text(size = 16))
 plot_s_eff_situ_space
-ggsave(plot_s_eff_situ_space, file = here("fig/mmPlots/plot_s_eff_situ_space.png"), width = 7, height = 6)
+#ggsave(plot_s_eff_situ_space, file = here("fig/mmPlots/plot_s_eff_situ_space.png"), width = 7, height = 6)
 
 s_emt_situ_space <- emmeans::emtrends(strength_mod, "situ", var = "space_use") %>%
   as.data.frame() %>%
@@ -290,7 +290,7 @@ plot_s_emt_situ_space <- s_emt_situ_space %>%
   as.data.frame() %>%
   ggplot(aes(x = situ, y = space_use.trend, col = situ))+
   geom_point(size = 6)+
-  geom_errorbar(aes(x = situ, ymin = asymp.LCL, ymax = asymp.UCL), 
+  geom_errorbar(aes(x = situ, ymin = lower.CL, ymax = upper.CL), 
                 width = 0, linewidth = 2)+
   geom_hline(aes(yintercept = 0), linetype = 2)+
   scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
@@ -299,82 +299,4 @@ plot_s_emt_situ_space <- s_emt_situ_space %>%
   xlab("Situation")+
   coord_flip()
 plot_s_emt_situ_space
-ggsave(plot_s_emt_situ_space, file = here("fig/mmPlots/plot_s_emt_situ_space.png"), width = 5, height = 6)
-
-## centr (main) -----------------------------------------------------------
-s_eff_centr <- as.data.frame(ggeffect(strength_mod, terms = "centr"))
-plot_s_eff_centr <- ggplot(s_eff_centr, aes(x, predicted))+
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high),
-              alpha = 0.2, linewidth = 0.6, show.legend = F)+
-  geom_point(data = linked, aes(x = centr, y = normStrength, col = situ), alpha = 0.5)+
-  geom_line(linewidth = 1)+
-  scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
-  ylab("Strength (normalized)")+
-  xlab("Spatial centrality")+
-  ggtitle("")+theme_classic()+
-  theme(text = element_text(size = 16))
-plot_s_eff_centr
-ggsave(plot_s_eff_centr, file = here("fig/mmPlots/plot_s_eff_centr.png"), width = 7, height = 6)
-
-## movement (main) ---------------------------------------------------------
-s_eff_movement <- as.data.frame(ggeffect(strength_mod, terms = "movement"))
-plot_s_eff_movement <- ggplot(s_eff_movement, aes(x, predicted))+
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high),
-              alpha = 0.2, linewidth = 0.6, show.legend = F)+
-  geom_point(data = linked, aes(x = movement, y = normStrength, col = situ), alpha = 0.5)+
-  geom_line(linewidth = 1, linetype = 2)+
-  scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
-  ylab("Strength (normalized)")+
-  xlab("Movement")+
-  ggtitle("")+theme_classic()+
-  theme(text = element_text(size = 16))
-plot_s_eff_movement
-ggsave(plot_s_eff_movement, file = here("fig/mmPlots/plot_s_eff_movement.png"), width = 7, height = 6)
-
-## roost_div (main) --------------------------------------------------------
-s_eff_roost <- as.data.frame(ggeffect(strength_mod, terms = "roost_div"))
-plot_s_eff_roost <- ggplot(s_eff_roost, aes(x, predicted))+
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high),
-              alpha = 0.2, linewidth = 0.6, show.legend = F)+
-  geom_point(data = linked, aes(x = roost_div, y = normStrength, col = situ), alpha = 0.5)+
-  geom_line(linewidth = 1)+
-  scale_color_manual(name = "Situation", values = c(cc$feedingColor, cc$flightColor, cc$roostingColor))+
-  ylab("Strength (normalized)")+
-  xlab("Roost diversification")+
-  ggtitle("")+theme_classic()+
-  theme(text = element_text(size = 16))
-plot_s_eff_roost
-ggsave(plot_s_eff_roost, file = here("fig/mmPlots/plot_s_eff_roost.png"), width = 7, height = 6)
-
-# Correlations between predictors -----------------------------------------
-
-centr_movement <- linked %>%
-  ggplot(aes(x = centr, y = movement, col = seasonUnique))+
-  geom_point()+
-  geom_smooth(method = "lm")+
-  ylab("Movement")+
-  xlab("Spatial centrality")+
-  theme(axis.title = element_text(size = 16))+
-  labs(color = "Season")
-
-centr_space_use <- linked %>%
-  ggplot(aes(x = centr, y = space_use, col = seasonUnique))+
-  geom_point()+
-  geom_smooth(method = "lm")+
-  ylab("Space use")+
-  xlab("Spatial centrality")+
-  theme(axis.title = element_text(size = 16))+
-  labs(color = "Season")
-
-centr_roost_div <- linked %>%
-  ggplot(aes(x = centr, y = roost_div, col = seasonUnique))+
-  geom_point()+
-  geom_smooth(method = "lm")+
-  ylab("Roost diversification")+
-  xlab("Spatial centrality")+
-  theme(axis.title = element_text(size = 16))+
-  labs(color = "Season")
-
-ggsave(centr_movement, file = here("fig/centr_movement.png"), width = 7, height = 4)
-ggsave(centr_space_use, file = here("fig/centr_space_use.png"), width = 7, height = 4)
-ggsave(centr_roost_div, file = here("fig/centr_roost_div.png"), width = 7, height = 4)
+#ggsave(plot_s_emt_situ_space, file = here("fig/mmPlots/plot_s_emt_situ_space.png"), width = 5, height = 6)
