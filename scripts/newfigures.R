d726c525662b45434147e9e3303658301ce842cc
Kaija Gahm <kaija.gahm@gmail.com>
2024-07-16 19:15:22 -0700

    components of Figure 2

--- scripts/newfigures.R
+++ scripts/newfigures.R
@@ -38,12 +38,12 @@ fig_2a <- linked %>%
   geom_density(linewidth = 1.5, col = situcolors[1])+
   theme_classic()+
   theme(legend.position = "bottom",
-        strip.text = element_text(size = 14),
-        text = element_text(family = "Verdana"),
+        text = element_text(family = "Verdana", size = 14),
         axis.title.y = element_blank())+
-  xlab("Observed degree\n(normalized)")
+  xlab("Observed degree\n(normalized)")+
+  ylab(" ")
 fig_2a
-ggsave(filename = here("fig/2a.png"), plot = fig_2a, width = 2.5, height = 2.5)
+ggsave(filename = here("fig/2a.png"), plot = fig_2a, width = 2.75, height = 2.75)
 
 ## B
 fig_2b <- linked %>%
@@ -53,12 +53,12 @@ fig_2b <- linked %>%
   geom_density(linewidth = 1.5, col = situcolors[1])+
   theme_classic()+
   theme(legend.position = "bottom",
-        strip.text = element_text(size = 14),
-        text = element_text(family = "Verdana"),
+        text = element_text(family = "Verdana", size = 14),
         axis.title.y = element_blank())+
-  xlab("Intentional degree\n(z-score)")
+  xlab("Intentional degree\n(z-score)")+
+  ylab(" ")
 fig_2b
-ggsave(filename = here("fig/2b.png"), plot = fig_2b, width = 2.5, height = 2.5)
+ggsave(filename = here("fig/2b.png"), plot = fig_2b, width = 2.75, height = 2.75)
 
 ## C. Correlation between measured and intentional sociality
 fig_2c <- linked %>%
@@ -71,7 +71,7 @@ fig_2c <- linked %>%
   geom_smooth(method = "lm", col = situcolors[1])+
   ylab("Intentional") + xlab("Observed")
 fig_2c # we see that these are correlated, but not perfectly. In particular, individuals with low normalized degree
-ggsave(filename = here("fig/2c.png"), fig_2c, width = 3, height = 2.5)
+ggsave(filename = here("fig/2c.png"), fig_2c, width = 2.75, height = 2.75)
 
 ## D. Example scatter and forest plots
 deg_movement_situ <- as.data.frame(ggeffect(deg_mod, 
@@ -81,26 +81,51 @@ deg_z_movement_situ <- as.data.frame(ggeffect(deg_z_mod,
                                               terms = c("movement", "situ"))) %>%
   mutate(mod = "deg_z")
 
+eff1 <- as.data.frame(emmeans::emtrends(deg_mod, specs = "situ", var = "movement")) %>% 
+  mutate(situ = case_when(situ == "Fe" ~ "Feeding",
+                          situ == "Fl" ~ "Flight",
+                          situ == "Ro" ~ "Roosting")) %>%
+  mutate(mod = "Observed")
+eff2 <- as.data.frame(emmeans::emtrends(deg_z_mod, specs = "situ", var = "movement")) %>% 
+  mutate(situ = case_when(situ == "Fe" ~ "Feeding",
+                          situ == "Fl" ~ "Flight",
+                          situ == "Ro" ~ "Roosting")) %>%
+  mutate(mod = "Intentional")
+
+eff <- bind_rows(eff1, eff2) %>%
+  mutate(mod = factor(mod, levels = c("Observed", "Intentional")))
+
+
+annot <- eff %>%
+  mutate(sig = case_when(lower.CL < 0 & upper.CL < 0 ~ T,
+                         lower.CL > 0 & upper.CL > 0 ~ T,
+                         .default = F)) %>%
+  select(situ, mod, sig)
+
 # combine the data and scale it
 dat <- bind_rows(deg_movement_situ, deg_z_movement_situ)
 mn_moddeg <- (deg_movement_situ %>% select(predicted, conf.low, conf.high) %>% rowSums() %>% sum())/(3*nrow(deg_movement_situ))
 mn_moddeg_z <- (deg_z_movement_situ %>% select(predicted, conf.low, conf.high) %>% rowSums() %>% sum())/(3*nrow(deg_z_movement_situ))
 
 dat <- dat %>%
-  mutate(mn = ifelse(mod == "deg", mn_moddeg, mn_moddeg_z)) %>%
+  mutate(mod = case_when(mod == "deg" ~ "Observed",
+                         mod == "deg_z" ~ "Intentional")) %>%
+  mutate(mn = ifelse(mod == "Observed", mn_moddeg, mn_moddeg_z)) %>%
   mutate(predicted_scl = predicted-mn,
          conf.low_scl = conf.low-mn,
          conf.high_scl = conf.high-mn) %>%
   mutate(group = case_when(group == "Fe" ~ "Feeding",
                            group == "Fl" ~ "Flight",
                            group == "Ro" ~ "Roosting")) %>%
-  mutate(mod = case_when(mod == "deg" ~ "Observed",
-                         mod == "deg_z" ~ "Intentional"))
+  left_join(annot, by = c("group" = "situ",
+                          "mod")) %>%
+  mutate(mod = factor(mod, levels = c("Observed", "Intentional")))
 
 fig_2d <- dat %>%
   ggplot(aes(x = x, y = predicted, group = group))+
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = group, col = NULL), alpha = 0.1)+
-  geom_line(linewidth = 1.5, aes(col = group))+
+  geom_line(linewidth = 1.5, aes(col = group, linetype = sig))+
+  scale_linetype_manual(values = c(2, 1), guide = "none")+
   facet_wrap(~mod, scale = "free_y")+
   scale_color_manual(name = "Situation",
                      values = situcolors)+
@@ -108,46 +133,33 @@ fig_2d <- dat %>%
                     values = situcolors)+
   xlab("Movement")+
   ylab("Model prediction")+
-  theme(text = element_text(family = "Verdana", size = 14))
+  theme(text = element_text(family = "Verdana", size = 14), 
+        legend.position = "bottom")
 fig_2d
+ggsave(filename = here("fig/2d.png"), fig_2d, width = 6, height = 4)
 
-## D. Example forest plot with both types of lines
-
-dat1 <- as.data.frame(emmeans::emtrends(deg_mod, specs = "situ", var = "movement")) %>% 
-  mutate(situ = case_when(situ == "Fe" ~ "Feeding",
-                          situ == "Fl" ~ "Flight",
-                          situ == "Ro" ~ "Roosting")) %>%
-  mutate(mod = "Normalized")
-dat2 <- as.data.frame(emmeans::emtrends(deg_z_mod, specs = "situ", var = "movement")) %>% 
-  mutate(situ = case_when(situ == "Fe" ~ "Feeding",
-                          situ == "Fl" ~ "Flight",
-                          situ == "Ro" ~ "Roosting")) %>%
-  mutate(mod = "Intentional")
-dat <- bind_rows(dat1, dat2)
-
-fig_2d <- dat %>%
+## E. Example forest plot with both types of lines
+fig_2e <- eff %>%
   ggplot(aes(x = situ, y = movement.trend, col = situ,
              group = interaction(mod, situ)))+
   geom_hline(aes(yintercept = 0), linetype = 2)+
   geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL),
                 width = 0, linewidth = 0.5,
                 position = position_dodge(width = 0.2))+
-  geom_point(size = 3, aes(pch = mod),
+  geom_point(size = 3, 
              position = position_dodge(width = 0.2),
              fill = "white")+
   scale_color_manual(values = situcolors, guide = "none")+
   scale_x_discrete(limits = rev, position = "top")+
   scale_shape_manual(name = "", values = c(16, 21))+
-  coord_flip() + ylab("Movement effect")+
-  guides(pch = guide_legend(position = "inside"))+
+  coord_flip() + 
+  facet_wrap(~mod, scales = "free",
+             ncol = 1)+
+  ylab("Movement effect")+
   theme(text = element_text(size = 14),
-        legend.position.inside = c(0.2, 0.15),
-        axis.title.y = element_blank(),
-        legend.text = element_text(size = 10),
-        legend.background = element_rect(fill = NA))
-fig_2d
-ggsave(filename = here("fig/2d.png"), fig_2d, width = 3.5, height = 4)
-
+        axis.title.y = element_blank())
+fig_2e
+ggsave(filename = here("fig/2e.png"), fig_2e, width = 3, height = 4)
 
 # Figure 3 ----------------------------------------------------------------
 deg_mov <- as.data.frame(emmeans::emtrends(deg_mod, specs = "situ", var = "movement")) %>% 
