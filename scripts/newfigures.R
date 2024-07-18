1d7e2a00cfe7422ec0930b9a70b3649cd62bc079
Kaija Gahm <kaija.gahm@gmail.com>
2024-07-17 17:06:03 -0700

    New version of forest plot figure

--- scripts/newfigures.R
+++ scripts/newfigures.R
@@ -156,7 +156,7 @@ fig_2e <- eff %>%
   facet_wrap(~mod, scales = "free",
              ncol = 1)+
   ylab("Movement effect")+
-  theme(text = element_text(size = 14),
+  theme(text = element_text(family = "Verdana", size = 14),
         axis.title.y = element_blank())
 fig_2e
 ggsave(filename = here("fig/2e.png"), fig_2e, width = 3, height = 4)
@@ -181,7 +181,8 @@ prepdata_forforestplots <- function(variable, mod, z_mod){
     mutate(mod = factor(mod, levels = c("Observed", "Intentional"))) %>%
     mutate(situ = case_when(situ == "Fe" ~ "Feeding",
                             situ == "Fl" ~ "Flight",
-                            situ == "Ro" ~ "Roosting"))
+                            situ == "Ro" ~ "Roosting"),
+           var = variable)
   
   annot <- eff %>%
     mutate(sig = case_when(lower < 0 & upper < 0 ~ T,
@@ -204,8 +205,14 @@ str_space <- prepdata_forforestplots(variable = "space_use", mod = str_mod,
   mutate(measure = "strength")
 forforest <- list(deg_movement, deg_space, str_movement, str_space)
 forforest <- map(forforest, ~.x %>% rename("trend" = 2))
+forforest_df <- purrr::list_rbind(forforest) %>%
+  mutate(var = case_when(var == "movement" ~ "Movement",
+                         var == "space_use" ~ "Space use"))
 
 plots <- map(forforest, ~{
+  var <- str_to_sentence(str_replace(.x$var[1], "_", " "))
+  measure <- str_to_sentence(.x$measure[1])
+  title <- paste0(var, " effect")
   p <- .x %>%
     ggplot(aes(x = trend, y = situ, col = situ,
                group = interaction(mod, situ)))+
@@ -221,15 +228,77 @@ plots <- map(forforest, ~{
     scale_shape_manual(name = "", values = c(16, 21))+
     facet_wrap(~mod, scales = "free_x",
                nrow = 1)+
-    xlab("Effect")+
+    xlab(title)+
     theme(text = element_text(size = 14),
           axis.title.y = element_blank())
   return(p)
 })
 
-patchwork <- (plots[[1]] + plots[[2]]) / (plots[[3]] + plots[[4]])
-patchwork + plot_annotation(
-  title = 'Forest plots for model results',
-  subtitle = 'test subtitle',
-  caption = 'test caption'
-)
+patchwork <- ((plots[[1]]+
+                 ggtitle("Degree")+
+                 theme(axis.text.y = element_blank(),
+                       axis.ticks.y = element_blank(),
+                       plot.title = element_text(hjust = 0.5),
+                       axis.title.x = element_blank())) + 
+                (plots[[2]] + ggtitle("Strength")+
+                   theme(plot.title = element_text(hjust = 0.5),
+                         axis.title.x = element_blank()))) / 
+  ((plots[[3]] + 
+      ggtitle("")+
+      theme(axis.text.y = element_blank(),
+            axis.ticks.y = element_blank(),
+            axis.title.x = element_text(size = 14))) + 
+     (plots[[4]] +
+        theme(axis.title.x = element_text(size = 14))))
+patchwork
+ggsave(patchwork, filename = here("fig/3a.png"), width = 7, height = 6)
+
+# But I don't like this because it allows each of the axes to vary. We need to have one set of axes for each model. We have a total of four models--degree and strength, observed and intentional. That means we need four different axes. But we can, and (maybe) should, but the effect sizes on the same axes for movement and space, since those are covariates in the same model. People are used to seeing that for forest plots, and it makes sense.
+
+# Let's give that a shot. There will be two plots, with two panels each, for a total of 4 panels.
+deg <- forforest_df %>%
+  filter(measure == "degree") %>%
+  ggplot(aes(x = trend, y = situ, col = situ,
+             group = interaction(mod, situ, var)))+
+  geom_vline(aes(xintercept = 0), linetype = 2)+
+  geom_errorbar(aes(xmin = lower, xmax = upper),
+                linewidth = 1,
+                width = 0, position = position_dodge(width = 0.3))+
+  geom_point(aes(pch = var), size = 5,
+             position = position_dodge(width = 0.3),
+             fill = "white")+
+  facet_wrap(~mod, scales = "free_x")+
+  scale_color_manual(values = situcolors, guide = F)+
+  scale_shape_manual(name = "Predictor", values = c(19, 21))+
+  ylab(NULL)+xlab("Effect size")+
+  ggtitle("Degree")+
+  scale_y_discrete(limits = rev)+
+  theme(text = element_text(family = "Verdana", size = 14),
+        legend.position = "none")
+
+str <- forforest_df %>%
+  filter(measure == "strength") %>%
+  ggplot(aes(x = trend, y = situ, col = situ,
+             group = interaction(mod, situ, var)))+
+  geom_vline(aes(xintercept = 0), linetype = 2)+
+  geom_errorbar(aes(xmin = lower, xmax = upper),
+                linewidth = 1,
+                width = 0, position = position_dodge(width = 0.3))+
+  geom_point(aes(pch = var), size = 5,
+             position = position_dodge(width = 0.3),
+             fill = "white")+
+  facet_wrap(~mod, scales = "free_x")+
+  scale_color_manual(values = situcolors, guide = F)+
+  scale_shape_manual(name = "Predictor", values = c(19, 21))+
+  ylab(NULL)+xlab("Effect size")+
+  ggtitle("Strength")+
+  scale_y_discrete(limits = rev)+
+  theme(text = element_text(family = "Verdana", size = 14),
+        legend.position = "right",
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank())
+
+patchwork_new <- deg + str
+patchwork_new
+ggsave(patchwork_new, filename = here("fig/3a.png"), width = 13, height = 6)
+# Ok yeah this version is much much more readable and way simpler and also more correct! Four sets of axes for four different models, no need to repeat them.
